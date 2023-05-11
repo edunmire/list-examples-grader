b9bd4e2a8875adebca91acdd85a8e5a2d199c4c5
edunmire <edunmire@ucsd.edu>
2023-05-11 11:50:00 -0700

    made changes to grade.sh
    grade.sh will now show if:
    1) file was found
    2) code compiled
    3) tests passed

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,11 +1,21 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 class IsMoon implements StringChecker {
   public boolean checkString(String s) {
     return s.equalsIgnoreCase("moon");
+  }
+}
+
+class LongStringChecker implements StringChecker {
+  public boolean checkString(String s) {
+      if(s.length()>4) {
+          return true;
+      }
+      return false;
   }
 }
 
@@ -18,4 +28,66 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test(timeout = 500)
+  public void testFilter_Empty() {
+    List<String> input = new ArrayList<>();
+    List<String> test = new ArrayList<>();
+    assertEquals(test, ListExamples.filter(input, new LongStringChecker()));
+  }
+
+  @Test(timeout = 500)
+  public void testFilter_OneVal() {
+    List<String> input = new ArrayList<>();
+    input.add("hello");
+    List<String> test = new ArrayList<>();
+    test.add("hello");
+    assertEquals(test, ListExamples.filter(input, new LongStringChecker()));
+  }
+
+  @Test(timeout = 500)
+  public void testFilter_MultVal() {
+    List<String> input = new ArrayList<>();
+    input.add("hello");
+    input.add("hi");
+    input.add("hey");
+    input.add("bonjour");
+    input.add("hola");
+    List<String> test = new ArrayList<>();
+    test.add("hello");
+    test.add("bonjour");
+    assertEquals(test, ListExamples.filter(input, new LongStringChecker()));
+  }
+
+  @Test(timeout = 500)
+  public void testMerge_Flipped() {
+    List<String> input1 = new ArrayList<>();
+    input1.add("e");
+      input1.add("t");
+      List<String> input2 = new ArrayList<>();
+      input2.add("a");
+      input2.add("g");
+      List<String> test = new ArrayList<>();
+      test.add("a");
+      test.add("e");
+      test.add("g");
+      test.add("t");
+      assertEquals(test, ListExamples.merge(input1, input2));
+    }
+  
+  @Test(timeout = 500)
+    public void testMerge_AllOneThenOther() {
+        List<String> input1 = new ArrayList<>();
+        input1.add("a");
+        input1.add("b");
+        List<String> input2 = new ArrayList<>();
+        input2.add("c");
+        input2.add("d");
+        List<String> test = new ArrayList<>();
+        test.add("a");
+        test.add("b");
+        test.add("c");
+        test.add("d");
+        assertEquals(test, ListExamples.merge(input1, input2));
+    }
 }
--- grade.sh
+++ grade.sh
@@ -1,4 +1,6 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+set -e
+
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -6,8 +8,46 @@ rm -rf grading-area
 mkdir grading-area
 
 git clone $1 student-submission
+
+
+cd student-submission
+
+if [[ -f ListExamples.java ]]
+then
+    echo "File exists"
+else
+    echo "File does not exist"
+    exit
+fi
+
 echo 'Finished cloning'
 
+cp ListExamples.java ../grading-area
+cd ..
+cp *.java ./grading-area
+cp -r lib ./grading-area
+cd grading-area
+
+set +e
+
+javac -cp $CPATH ListExamples.java TestListExamples.java >compile-output.txt 2>&1
+if [[ $? -eq 0 ]]
+then
+    echo "Code compiles"
+else
+    echo "Code does not compile"
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples >run-output.txt 2>&1
+
+err_code=$?
+
+if [[ $err_code -eq 0 ]]
+then
+    echo "Tests all passed!"
+else
+    echo "Something didn't work!"
+fi
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
